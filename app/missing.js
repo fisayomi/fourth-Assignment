e8154eae0bf40d10f70b2802f183327c1c79866f
Fisayomi <fisayomi@users.noreply.github.com>
2016-11-30 16:23:09 +0100

    Made few changes

--- app/missing.js
+++ app/missing.js
@@ -1,4 +1,5 @@
 function findMissing(arr1,arr2){
+  // a function that add all elements in a given array
   function add(array){
     var count = 0;
     for(var i = 0; i < array.length; i++){
@@ -7,14 +8,14 @@ function findMissing(arr1,arr2){
     return count;
   }
   
-  var val1 = add(arr1);
-  var val2 = add(arr2);
+  var arr1Numbers = add(arr1);
+  var arr2Numbers = add(arr2);
   
-  if(val2 > val1){
-    return val2 - val1;
+  if(arr2Numbers > arr1Numbers){
+    return arr2Numbers - arr1Numbers;
   }
   
-  else return val1 - val2;
+  else return arr1Numbers - arr2Numbers;
 }
 
 module.exports = {findMissing:findMissing}
